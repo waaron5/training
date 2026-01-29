7007baeee4307f59c7c3083cadacba0cb28315cb
waaron5 <aarondnwd@gmail.com>
2026-01-29 21:52:55 +0000

    Admin Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,24 @@ import {
   AuthErrorPage,
   ForbiddenPage,
   NotFoundPage,
+  permissionGuard,
   ServerErrorPage,
 } from '@fhss-web-team/frontend-utils';
 import { HomePage } from './pages/home/home.page';
 import { DefaultLayout } from './layouts/default/default.layout';
+import { AdminPage } from './pages/admin/admin.page';
+import { Permission } from '../security';
 
 export const routes: Routes = [
   {
     path: '',
     component: DefaultLayout,
     children: [
+      {
+        path: 'admin',
+        component: AdminPage,
+        canActivate: [permissionGuard<Permission>(['view-secrets'])],
+      },
       { path: 'server-error', component: ServerErrorPage },
       { path: 'forbidden', component: ForbiddenPage },
       { path: 'auth-error', component: AuthErrorPage },
---
+++ src/app/pages/admin/admin.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.page.html',
+  styleUrl: './admin.page.scss',
+})
+export class AdminPage {
+
+}
